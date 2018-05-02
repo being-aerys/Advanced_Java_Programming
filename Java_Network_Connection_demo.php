c370cba7da1de51d71f8d1fc4e00a7bd6a1a8554
intelaashish <aashish73134@gmail.com>
2018-05-02 22:59:14 +0545

    URL CONNECTION AND PARSING THE RETURNED DATA

---
+++ Java_Network_Connection_demo.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['type'])){
+
+$type=$_POST['type'];
+
+if($type=="Java_Program_Demo"){
+	$msg="Mount Annapurna Campus]Pokhara]Nepal]BSc CsIT]Advanced Java Programming]7th Semester";
+	echo $msg;
+	
+	
+}
+}
+	
+?>
