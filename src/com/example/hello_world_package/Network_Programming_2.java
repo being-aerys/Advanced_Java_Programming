c83c0be2880761ac1af442574400b3197993ecf7
intelaashish <aashish73134@gmail.com>
2018-05-02 20:32:31 +0545

    updated code

--- src/com/example/hello_world_package/Network_Programming_2.java
+++ src/com/example/hello_world_package/Network_Programming_2.java
@@ -13,8 +13,18 @@ import java.net.*;
                 System.out.println("Protocol: " + url.getProtocol());
                 System.out.println("Host Name: " + url.getHost());
                 System.out.println("Port Number: " + url.getPort()); //returns -1 since no port no specified by us in the url
+
                 System.out.println("Port Number: " + url.getDefaultPort());//returns default port no for the protocol here for HTTPS 443
+
                 System.out.println("File Name: " + url.getFile());
+
+                //A URL can optionally specify a "port", which is the port number to which the TCP connection is made on the remote host machine. If the port is not specified, the default port for the protocol is used instead. For example, the default port for http is 80. An alternative port could be specified as:
+
+                URL url_next = new URL("http://www.facebook.com:1010/docs/resource1.html");
+                System.out.println("Port Number for url_next: " + url_next.getPort());
+
+
+
 
             } catch (Exception e) {
                 System.out.println(e);
